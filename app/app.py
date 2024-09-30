8bfd1ea786ef05b655850dce2cd7402b184e0481
Willian Alves Lima <willian.lima@bb.com.br>
2024-09-30 19:19:27 -0300

    refatorar consultas

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 """API simples."""
-from fastapi import FastAPI, HTTPException, status
+from fastapi import FastAPI
 
-from app.constants import (URL_BASE_COMERCIALIZACAO, URL_BASE_EXPORTACAO,
-                           URL_BASE_IMPORTACAO, URL_BASE_PROCESSAMENTO,
-                           URL_BASE_PRODUCAO)
-from app.utils import convertar_para_json, ler_base_dados_csv
+from app.constants import BASE
+from app.utils import consultar_base, erro_500
 
 app = FastAPI()
 
@@ -15,74 +13,53 @@ app = FastAPI()
 async def consultar_producao() -> str:
     """Consulta da Base de Produção, retorna os dados em formato JSON."""
     try:
-        df_producao = ler_base_dados_csv(URL_BASE_PRODUCAO)
-        return convertar_para_json(df_producao)
+        return consultar_base(BASE.PRODUCAO)
     except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Erro ao ao consultar a base de Produção: {str(e)}",
-        )
-
-
-@app.get("/processamento", summary="Consulta a Base de Processamento",
-         description=("Consulta a Base de Processamento de vinhos da Empraba "
-                      "e retorna os dados em formato JSON."))
-async def consultar_processamento() -> str:
-    """Consulta da Base de Processamento, retorna os dados em formato JSON."""
-    try:
-        df_processamento = ler_base_dados_csv(URL_BASE_PROCESSAMENTO)
-        return convertar_para_json(df_processamento)
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Erro ao ao consultar a base de Processamento: {str(e)}",
-        )
+        erro_500(e, BASE.PRODUCAO)
 
 
 @app.get("/comercializacao", summary="Consulta a Base de Comercialização",
-         description="Consulta a Base de Comercialização de vinhos da Empraba"
-         "e retorna os dados em formato JSON.")
+         description="Consulta a Base de Comercialização de vinhos " +
+         "da Empraba e retorna os dados em formato JSON.")
 async def consultar_comercializacao() -> str:
     """consultar_comercializacao.
 
-    Consulta da Base de Comercialização,
-    retorna os dados em formato JSON.
+    Consulta da Base de Comercialização, retorna os dados em formato JSON.
     """
     try:
-        df_comercializacao = ler_base_dados_csv(URL_BASE_COMERCIALIZACAO)
-        return convertar_para_json(df_comercializacao)
+        return consultar_base(BASE.COMERCIALIZACAO)
     except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Erro ao ao consultar a base de Comercialização: {str(e)}",
-        )
-
-
-@app.get("/importacao", summary="Consulta a Base de Importação",
-         description="Consulta a Base de Importação de vinhos da Empraba"
-         " e retorna os dados em formato JSON.")
-async def consultar_importacao() -> str:
-    """Consulta da Base de Importação, retorna os dados em formato JSON."""
-    try:
-        df_importacao = ler_base_dados_csv(URL_BASE_IMPORTACAO)
-        return convertar_para_json(df_importacao)
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Erro ao ao consultar a base de Importação: {str(e)}",
-        )
+        erro_500(e, BASE.COMERCIALIZACAO)
 
 
 @app.get("/exportacao", summary="Consulta a Base de Exportação",
-         description="Consulta a Base de Exportação de vinhos da Empraba"
-         " e retorna os dados em formato JSON.")
+         description="Consulta a Base de Exportação de vinhos da Empraba " +
+         "e retorna os dados em formato JSON.")
 async def consultar_exportacao() -> str:
     """Consulta da Base de Exportação, retorna os dados em formato JSON."""
     try:
-        df_exportacao = ler_base_dados_csv(URL_BASE_EXPORTACAO)
-        return convertar_para_json(df_exportacao)
+        return consultar_base(BASE.EXPORTACAO)
     except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Erro ao ao consultar a base de Exportação: {str(e)}",
-        )
+        erro_500(e, BASE.EXPORTACAO)
+
+
+@app.get("/importacao", summary="Consulta a Base de Importação",
+         description="Consulta a Base de Importação de vinhos da Empraba " +
+         "e retorna os dados em formato JSON.")
+async def consultar_importacao() -> str:
+    """Consulta da Base de Importação, retorna os dados em formato JSON."""
+    try:
+        return consultar_base(BASE.IMPORTACAO)
+    except Exception as e:
+        erro_500(e, BASE.IMPORTACAO)
+
+
+@app.get("/processamento", summary="Consulta a Base de Processamento",
+         description="Consulta a Base de Processamento de vinhos da Empraba " +
+         "e retorna os dados em formato JSON.")
+async def consultar_processamento() -> str:
+    """Consulta da Base de Processamento, retorna os dados em formato JSON."""
+    try:
+        return consultar_base(BASE.PROCESSAMENTO)
+    except Exception as e:
+        erro_500(e, BASE.PROCESSAMENTO)
--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,8 @@
 """Funçoes auxiliares."""
 import pandas as pd
+from fastapi import HTTPException
+
+from app.constants import BASE
 
 
 def ler_base_dados_csv(url_base_dados_csv: str) -> pd.DataFrame:
@@ -7,24 +10,23 @@ def ler_base_dados_csv(url_base_dados_csv: str) -> pd.DataFrame:
 
     ler_base_dados_csv Realiza leitura de um arquivo CSV
     em uma URL e retorna um DataFrame.
-
-    Args:
-        url_base_dados_csv (str): URL do arquivo CSV.
-
-    Returns:
-        pd.DataFrame: DataFrame com os dados do arquivo CSV.
     """
     return pd.read_csv(url_base_dados_csv, sep=";")
 
 
 def convertar_para_json(df: pd.DataFrame) -> str:
-    """
-    convertar_para_json Converte um DataFrame em um JSON.
-
-    Args:
-        df (pd.DataFrame): DataFrame a ser convertido.
-
-    Returns:
-        str: JSON gerado a partir do DataFrame.
-    """
+    """convertar_para_json Converte um DataFrame em um JSON."""
     return df.to_json(orient="records")
+
+
+def consultar_base(base: BASE) -> str:
+    """Consulta a base de dados."""
+    return convertar_para_json(ler_base_dados_csv(base.url))
+
+
+def erro_500(e: Exception, base: BASE) -> HTTPException:
+    """erro_consultar_base."""
+    raise HTTPException(
+        status_code=500,
+        detail=f"Erro ao ao consultar a {base.descricao}: {str(e)}",
+    )
