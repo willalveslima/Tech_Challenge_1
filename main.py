6f2103b35404f95a35b15ff6d0a9034f03588e9e
Willian Alves Lima <w.alves.lima@gmail.com>
2024-11-03 22:15:34 -0300

    Alterar config unicorn

--- main.py
+++ main.py
@@ -95,4 +95,8 @@ app = create_app()
 
 logger.debug("Aplicação criada com sucesso!")
 
-uvicorn.run(app)
+# uvicorn.run(app)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+
